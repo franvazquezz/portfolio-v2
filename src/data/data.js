50ddc9d96c9063a6d57049a8a3aec14faa355a83
franvazquezz <franvazquez2001@gmail.com>
2023-08-16 15:36:46 -0300

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+export const data = [
+	{
+		id: 1,
+		img: "https://i.postimg.cc/XvD2PgDm/logo192.png",
+		title: "Gym Bro",
+		desc: "Short project, it occurred to me when I went to the gym and I had the plan in an Excel spreadsheet, it is a SPA in order to be able to see the gym plan. It is made with React and Tailwind CSS, the next step is to create users to make it more scalable.",
+		link: "https://gym-vert.vercel.app/",
+		git: "https://github.com/franvazquezz/gym",
+	},
+	{
+		id: 2,
+		img: "https://i.postimg.cc/m2q0cBSK/fondohome.jpg",
+    title: "Creative Canines",
+    desc: "This is Henry's Individual Project, which is a SPA, working with Vite, React, Redux, Express, Node, Sequelize and PostgreSQL. The languages that work in these projects are JavaScript, CSS and HTML.",
+		link: "https://creative-canines-fv.vercel.app/",
+		git: "https://github.com/franvazquezz/PI-Henry-Dogs",
+	},
+];
